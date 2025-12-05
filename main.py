5c64ff46ef1cc884fa3f774086d726577adb0853
Fran <hopeli919@gmail.com>
2025-12-04 20:37:53 -0800

    update cors

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ app.include_router(authors.router)   # /authors/...
 # ---------------------------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://vm-social.vercel.app"],   
+    allow_origins=["https://vm-social.vercel.app", "https://viewmim.vercel.app"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
